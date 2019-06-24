426a78fd12dd053196e62cdba97dbcdbc5199ca9
1036225283 <id512@126.com>
2019-06-24 20:38:35 +0800

    开始学习python

--- main/learn/np.py
+++ main/learn/np.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as m
 
 # 各个元素的平方
 x = np.square([2, 1, 1])
@@ -8,3 +9,10 @@ y = np.sum(x)
 
 print(x)
 print(y)
+
+print("1/2 = ", 1 / 2)
+
+print("函数的调用 = ", pow(2, 3))
+print("-20的绝对值 = ", abs(-20))
+
+print("导入包 = ", m.cos(12))
